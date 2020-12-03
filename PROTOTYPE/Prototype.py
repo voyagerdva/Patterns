de808de4183ec0cf3c99160e4c24b3daa3d29bb0
voyagerdva <mariner-9@yandex.ru>
2020-12-03 19:38:21 +0300

    Prototype begin

---
+++ PROTOTYPE/Prototype.py
@@ -0,0 +1,15 @@
+import copy
+
+class Prototype:
+    _type = None
+    _value = None
+    
+    def clone(self):
+        pass
+    
+    def getType(self):
+        return self._type
+    
+    def getValue(self):
+        return self._value
+    
