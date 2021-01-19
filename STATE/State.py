c41c74eed96e0ae4913f58240c78dca9505067ec
voyagerdva <mariner-9@yandex.ru>
2021-01-19 23:12:57 +0300

    state some changes

--- STATE/State.py
+++ STATE/State.py
@@ -3,32 +3,14 @@ from abc import ABC, abstractmethod
 
 
 class Context:
-    """
-    Контекст определяет интерфейс, представляющий интерес для клиентов. Он также
-    хранит ссылку на экземпляр подкласса Состояния, который отображает текущее
-    состояние Контекста.
-    """
-
     _state = None
-    """
-    Ссылка на текущее состояние Контекста.
-    """
-
     def __init__(self, state: State) -> None:
         self.transition_to(state)
 
     def transition_to(self, state: State):
-        """
-        Контекст позволяет изменять объект Состояния во время выполнения.
-        """
-
         print(f"Context: Transition to {type(state).__name__}")
         self._state = state
         self._state.context = self
-
-    """
-    Контекст делегирует часть своего поведения текущему объекту Состояния.
-    """
 
     def request1(self):
         self._state.handle1()
@@ -38,13 +20,6 @@ class Context:
 
 
 class State(ABC):
-    """
-    Базовый класс Состояния объявляет методы, которые должны реализовать все
-    Конкретные Состояния, а также предоставляет обратную ссылку на объект
-    Контекст, связанный с Состоянием. Эта обратная ссылка может использоваться
-    Состояниями для передачи Контекста другому Состоянию.
-    """
-
     @property
     def context(self) -> Context:
         return self._context
@@ -60,13 +35,6 @@ class State(ABC):
     @abstractmethod
     def handle2(self) -> None:
         pass
-
-
-"""
-Конкретные Состояния реализуют различные модели поведения, связанные с
-состоянием Контекста.
-"""
-
 
 class ConcreteStateA(State):
     def handle1(self) -> None:
@@ -87,10 +55,7 @@ class ConcreteStateB(State):
         print("ConcreteStateB wants to change the state of the context.")
         self.context.transition_to(ConcreteStateA())
 
-
 if __name__ == "__main__":
-    # Клиентский код.
-
     context = Context(ConcreteStateA())
     context.request1()
     context.request2()
